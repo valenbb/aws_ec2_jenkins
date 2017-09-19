8743660680be366d25f035cefeaa4cd1dd874617
Andy Lalla <valenbb@gmail.com>
2017-09-18 21:25:50 -0400

    Updated main script

--- docker.tf
+++ docker.tf
@@ -3,6 +3,7 @@ provider "aws" {}
 resource "aws_instance" "docker" {
   ami           = "${var.aws_ami}"
   instance_type = "${var.instance_type}"
+  associate_public_ip_address = "true"
 
   tags {
     Name = "docker"
@@ -17,15 +18,14 @@ resource "aws_instance" "docker" {
   key_name          = "${var.key_name}"
   vpc_security_group_ids = ["${var.security_group}"]
   
-  connection {
-    user = "centos"
-  }
+  user_data = <<HEREDOC
+  #!/bin/bash
+  sudo yum update -y
+  sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+  sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+  sudo yum install docker-ce
+  sudo service docker start
+  sudo chkconfig docker on
+HEREDOC
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum update -y",
-      "sudo yum -y install nginx",
-      "sudo service nginx start",
-    ]
-  }
 }
