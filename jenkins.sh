46fecb0c47fc24d1372282ecac3440bb31ebcb00
Andy Lalla <valenbb@gmail.com>
2017-10-29 12:42:42 -0400

    Fixed issue with jenkins script

--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo yum update -y
-sudo yum install java-1.8.0
-sudo yum remove java-1.7.0-openjdk
+sudo yum install java-1.8.0 -y
+sudo yum remove java-1.7.0-openjdk -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
 sudo yum install jenkins -y
